bf1ba942b922658026e8576de4f8235f4b01807f
sikayla <kaylatizon5@gmail.com>
2025-06-07 14:36:22 +0800

    Add Render domain to ALLOWED_HOSTS

--- student_mgmt_full/settings.py
+++ student_mgmt_full/settings.py
@@ -3,7 +3,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-demo-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['final-project-0uik.onrender.com', 'localhost', '127.0.0.1']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
